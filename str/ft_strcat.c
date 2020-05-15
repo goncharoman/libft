8b5dd119d2e76e011681edd009c16c197301d718
Roman Gonchar <goncharoman@gmail.com>
2020-05-15 04:18:17 +0300

    little fix ft_strcat

--- str/ft_strcat.c
+++ str/ft_strcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ujyzene <ujyzene@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ujyzene <ujyzene@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 21:32:02 by ujyzene           #+#    #+#             */
-/*   Updated: 2019/04/05 02:07:20 by ujyzene          ###   ########.fr       */
+/*   Updated: 2020/05/14 18:32:22 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 char	*ft_strcat(char *s1, const char *s2)
 {
+	if (!s2)
+		return (NULL);
 	return (ft_strncat(s1, s2, ft_strlen(s2)));
 }
